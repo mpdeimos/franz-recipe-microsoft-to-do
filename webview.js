4f42b5934e9d3edf3e28c3f010209acd1f505794
Martin Pöhlmann <mpdeimos@users.noreply.github.com>
2019-01-25 22:26:44 +0100

    Add badge for unreolved tasks of the day

--- webview.js
+++ webview.js
@@ -1,3 +1,15 @@
 "use strict";
 
-module.exports = Franz => Franz;
+module.exports = Franz => {
+  function updateBadge() {
+    let myDayOpen = 0;
+    const myDay = document.querySelector(".todayToolbar-count");
+    if (myDay) {
+      myDayOpen = parseInt(myDay.innerHTML, 10);
+    }
+
+    Franz.setBadge(myDayOpen); 
+  }
+
+  Franz.loop(updateBadge);
+};
